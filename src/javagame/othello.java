9fb3b1168f5070da72a47869b62af037c7a5cbcf
Santosh reddy <rsantosh@iitrpr.ac.in>
2014-02-12 22:38:43 +0530

    first commit

---
+++ src/javagame/othello.java
@@ -0,0 +1,13 @@
+package javagame;
+
+public class othello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("kergnkjwrtnehkg");
+	}
+
+}
